6864169b343cb0578402c2771a88ffff705b1787
TianLangHin <tianlanghin@gmail.com>
2025-10-09 02:02:57 +1100

    Initial Commit

---
+++ PokeCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeCalc
+//
+//  Created by Tian Lang Hin on 9/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeCalc/PokeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCalcApp.swift
+//  PokeCalc
+//
+//  Created by Tian Lang Hin on 9/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
